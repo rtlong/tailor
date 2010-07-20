ec019459a9193f610eb136eca386a0be13ce0d5d
Steve Loveless <steve.loveless@gmail.com>
2010-07-19 23:45:48 -0700

    Updating new grammar... still testing

--- lib/tailor/grammars/test.rb
+++ lib/tailor/grammars/test.rb
@@ -1,19 +1,25 @@
-require 'spec_helper.rb'
 require 'treetop'
 require 'polyglot'
-require 'tailor/grammars/style'
+require './style'
 #require 'tailor/simple_html'
 require 'pp'
 
-f = File.open('../features/support/1_file_with_bad_comma_spacing/bad_comma_spacing.rb', 'rb')
+f = File.open('../../../features/support/1_file_with_bad_comma_spacing/bad_comma_spacing.rb', 'rb')
 text = f.read
 
 #text = "This is some text\nSecond line,bobo\n  Third line, meow"
 parser = StyleParser.new
 
 result = parser.parse text
-puts result.values if result
-result.values.each {|v| puts v[:values]}
+puts result
+puts result.inspect
+pp result.values
+#puts result.elements
+#puts result.elements.methods
+#puts result.elements.inspect
+#puts result.values if result
+#result.values.each {|v| puts v[:values]}
+=begin
 puts 'stuf...'
 puts result.line
 puts result.line.values
@@ -25,3 +31,4 @@ puts result.input
 puts result.interval
 puts result.parent
 puts result.elements
+=end
