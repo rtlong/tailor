19a4db943c7d9f10408fb9b30f10bbd477ee46c0
Steve Loveless <steve.loveless@gmail.com>
2010-08-01 21:31:05 -0700

    Removed #line_count and fixed tests

--- spec/grammars/ruby_file_spec.rb
+++ spec/grammars/ruby_file_spec.rb
@@ -2,27 +2,22 @@ require File.dirname(__FILE__) + '/../spec_helper.rb'
 require 'citrus'
 
 describe RubyFile do
+  it "should do what I say" do
+    r = RubyFile.parse("line 1\nline 2\n")
+    puts r
+    pp r
+  end
+
   context "line counting" do
-    it "should count 2 lines of code" do
-      r = RubyFile.parse("line 1\nline 2\n")
-      r.line_count.should == 2
-    end
-
-    it "should count 2 lines of code when no \\n appears at the end" do
-      r = RubyFile.parse("line 1\nline 2")
-      r.line_count.should == 2
-    end
-
     it "should count 2 lines of code with a bad string interpolation" do
       r = RubyFile.parse("\"This is \#{ foo }\"\ndef blah;end;")
-      r.line_count.should == 2
 
       s = r.find :double_quoted_string
       s.first.malformed?.should == true
     end
 
     it "should report problem and the line it's on" do
-      r = RubyFile.parse("\"This is \#{ foo }\"\ndef blah;end;")
+      r = RubyFile.parse("\"This is \#{ foo }\"\ndef blah;end;", :enable_memo => true)
       r.style_errors.length.should == 1
       r.style_errors.first[:line].should == 1
       r.style_errors.first[:problem_text].should == "\"This is \#{ foo }\""
@@ -46,12 +41,12 @@ describe RubyFile do
   context "reports problems" do
     it "should report a problem with bad string interpolation" do
       r = RubyFile.parse("\"This is \#{ foo }\"\ndef blah;end;")
-      r.style_errors.first.should == "\"This is \#{ foo }\""
+      r.style_errors.first[:problem_text].should == "\"This is \#{ foo }\""
     end
 
     it "should report 2 problems with bad string interpolation" do
-      r = RubyFile.parse("\"This is \#{ foo }\"\n\"This is \#{ bar}\";")
-      r.style_errors[1].should == "\"This is \#{ bar}\""
+      r = RubyFile.parse("\"This is \#{ foo }\"\n\"This is \#{ bar}\"")
+      r.style_errors[1][:problem_text].should == "\"This is \#{ bar}\""
     end
   end
 end
