0c1f99749d5aeed7fa10961c510a449e8a14571e
Steve Loveless <steve.loveless@gmail.com>
2010-08-04 22:02:22 -0700

    Added detection of long lines and removed extra tests

--- spec/grammars/ruby_file_spec.rb
+++ spec/grammars/ruby_file_spec.rb
@@ -13,6 +13,20 @@ describe RubyFile do
   end
 
   context "find Ruby types" do
+    it "should find a line" do
+      r = RubyFile.parse('s = "This is a string"')
+      lines = r.find(:line)
+      lines.class.should == Array
+      lines.length.should == 1
+    end
+
+    it "should find 2 lines" do
+      r = RubyFile.parse("s = \"This is a string\"\nt = String.new")
+      lines = r.find(:line)
+      lines.class.should == Array
+      lines.length.should == 2
+    end
+
     it "should find a string" do
       r = RubyFile.parse('s = "This is a string"')
       strings = r.find(:string)
@@ -36,67 +50,73 @@ describe RubyFile do
   end
 
   context "reports problems" do
-    it "should report a problem with bad string interpolation" do
-      r = RubyFile.parse("\"This is \#{ foo }\"\ndef blah;end;")
-      r.style_errors.first[:problem_text].should == "\"This is \#{ foo }\""
+    context "with strings" do
+      context "with bad interpolation" do
+        it "when on first line" do
+          r = RubyFile.parse("\"This is \#{ foo }\"\ndef blah;end;")
+          r.style_errors.first[:problem_text].should == "\"This is \#{ foo }\""
+          r.style_errors.first[:summary].should == "Bad string interpolation"
+        end
+
+        it "when on second line" do
+          r = RubyFile.parse("# Comment line\n\"This is \#{ foo }\"\ndef blah;end;")
+          r.style_errors.length.should == 1
+          r.style_errors.first[:line].should == 2
+          r.style_errors.first[:problem_text].should == "\"This is \#{ foo }\""
+        end
+
+        it "when on first and third line" do
+          r = RubyFile.parse("\"This is \#{ foo }\"\ndef blah;end;\n\"This is \#{ foo}\"")
+          r.style_errors.length.should == 2
+          r.style_errors[0][:line].should == 1
+          r.style_errors[0][:problem_text].should == "\"This is \#{ foo }\""
+          r.style_errors[1][:line].should == 3
+          r.style_errors[1][:problem_text].should == "\"This is \#{ foo}\""
+        end
+
+        it "when on second and fourth line" do
+          r = RubyFile.parse("# Comment Line\n\"This is \#{ foo }\"\ndef blah;end;\n\"This is \#{ foo}\"")
+          r.style_errors.length.should == 2
+          r.style_errors[0][:line].should == 2
+          r.style_errors[0][:problem_text].should == "\"This is \#{ foo }\""
+          r.style_errors[1][:line].should == 4
+          r.style_errors[1][:problem_text].should == "\"This is \#{ foo}\""
+        end
+
+        it "should report 0 problems with NO bad string interpolation" do
+          r = RubyFile.parse("\"This is \#{foo}\"\n\"This is \#{bar}\"")
+          r.style_errors.should be_empty
+        end
+      end
     end
 
-    it "should report 2 problems with bad string interpolation" do
-      r = RubyFile.parse("\"This is \#{ foo }\"\n\"This is \#{ bar}\"")
-      r.style_errors[1][:problem_text].should == "\"This is \#{ bar}\""
-    end
-
-    it "should report 0 problems with NO bad string interpolation" do
-      r = RubyFile.parse("\"This is \#{foo}\"\n\"This is \#{bar}\"")
-      r.style_errors.should be_empty
-    end
-
-    it "should report a hard tab at the beginning of the line" do
-      r = RubyFile.parse("\t\"This is \#{foo}\"")
-      r.style_errors.first[:problem_text].should == "\t"
-    end
-  end
-
-  context "reports problem summaries" do
-    it "should summarize a problem with bad string interpolation" do
-      s = "\"This is \#{ foo }\""
-      r = RubyFile.parse(s)
-      r.style_errors.first[:summary].should == "Bad string interpolation"
-    end
-  end
-
-  context "line counting AND problem reporting" do
-    context "should report problem and the line it's on" do
-      it "when on first line" do
-        r = RubyFile.parse("\"This is \#{ foo }\"\ndef blah;end;")
-        r.style_errors.length.should == 1
-        r.style_errors.first[:line].should == 1
-        r.style_errors.first[:problem_text].should == "\"This is \#{ foo }\""
+    context "with hard tabs" do
+      it "should report a hard tab at the beginning of the line" do
+        r = RubyFile.parse("\t\"This is \#{foo}\"")
+        r.style_errors.first[:problem_text].should == "\t"
+        r.style_errors.first[:summary].should == "[Spacing]  Line contains hard tabs"
       end
 
-      it "when on second line" do
-        r = RubyFile.parse("# Comment line\n\"This is \#{ foo }\"\ndef blah;end;")
-        r.style_errors.length.should == 1
-        r.style_errors.first[:line].should == 2
-        r.style_errors.first[:problem_text].should == "\"This is \#{ foo }\""
+      it "should NOT report 0 hard tabs at the beginning of the line" do
+        r = RubyFile.parse("\"This is \#{foo}\"")
+        r.style_errors.should be_empty
+      end
+    end
+
+    context "with lines" do
+      it "should report a line > 80 characters" do
+        s = ' ' * 81
+        r = RubyFile.parse(s)
+        r.malformed?.should be_true
+        r.style_errors.first[:problem_text].should == s
+        r.style_errors.first[:summary].should == "Line is >80 characters (81)"
       end
 
-      it "when on first and third line" do
-        r = RubyFile.parse("\"This is \#{ foo }\"\ndef blah;end;\n\"This is \#{ foo}\"")
-        r.style_errors.length.should == 2
-        r.style_errors[0][:line].should == 1
-        r.style_errors[0][:problem_text].should == "\"This is \#{ foo }\""
-        r.style_errors[1][:line].should == 3
-        r.style_errors[1][:problem_text].should == "\"This is \#{ foo}\""
-      end
-
-      it "when on second and fourth line" do
-        r = RubyFile.parse("# Comment Line\n\"This is \#{ foo }\"\ndef blah;end;\n\"This is \#{ foo}\"")
-        r.style_errors.length.should == 2
-        r.style_errors[0][:line].should == 2
-        r.style_errors[0][:problem_text].should == "\"This is \#{ foo }\""
-        r.style_errors[1][:line].should == 4
-        r.style_errors[1][:problem_text].should == "\"This is \#{ foo}\""
+      it "should NOT report a line == 80 characters" do
+        s = ' ' * 80
+        r = RubyFile.parse(s)
+        r.malformed?.should be_false
+        r.style_errors.should be_empty
       end
     end
   end
